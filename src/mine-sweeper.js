7658569bda2cddf30e79c7bd91e0f8b04935a03d
Evgeny Galitsky <e.galitsky@bk.ru>
2022-10-18 01:25:27 +0300

    feat: solve mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  matrix.forEach((e) => {
+    for (let i = 0; i < e.length; i++) {
+      if (e[i] === true && i !== 0) {
+        e[i + 1] = 1;
+        e[i - 1] = 1;
+        i++;
+      } else if (e[i] === true && i === 0) {
+        e[i + 1] = 1;
+        i++;
+      } else e[i] = 0;
+    }
+  });
+
+  const minesArr = matrix.map((e) => e.slice());
+
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[j][i] === true && j !== 0 && i !== 0) {
+        minesArr[j - 1][i + 1] += 1;
+        minesArr[j - 1][i - 1] += 1;
+        minesArr[j - 1][i] += 1;
+        minesArr[j + 1][i] += 1;
+        minesArr[j + 1][i - 1] += 1;
+        minesArr[j + 1][i + 1] += 1;
+      }
+      if (matrix[j][i] === true && j === 0 && i === 0) {
+        minesArr[j + 1][i] += 1;
+        minesArr[j + 1][i + 1] += 1;
+      }
+    }
+  }
+  for (let i = 0; i < matrix[0].length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      if (matrix[j][i] === true) minesArr[j][i] = 1;
+    }
+  }
+  return minesArr;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
